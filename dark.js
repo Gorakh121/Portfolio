fc03516749bac4492857976464375d17346c44b1
Prayas121 <prayasbaral9800@gmail.com>
2024-02-16 18:17:47 +0545

    Update

---
+++ dark.js
@@ -0,0 +1,5 @@
+// document.getElementById('mode').addEventListener('click', () => {
+//   const b = document.body;
+//   b.classList.toggle('light-mode');
+//   b.classList.toggle('dark-mode');
+// });
