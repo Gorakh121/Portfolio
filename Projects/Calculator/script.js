b032d77fa8ef4eb620efbec9f0545199f5a57067
Prayas121 <prayasbaral9800@gmail.com>
2024-02-26 07:44:45 +0545

    Logic added

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,47 +1,54 @@
-let allButtons = document.querySelectorAll("button");
+let arrayofoperator = ["+","-","*","/"];
 let input = document.getElementById("display");
-let currentInput = "";
-let previousInput = "";
-let operator = "";
-let result = "";
-
-allButtons.forEach((element) => {
+console.log(input)
+let Button = document.querySelectorAll("button");
+Button.forEach((element) => {
   element.addEventListener("click", () => {
-    const buttonValue = element.value;
-
-    switch (buttonValue) {
-      case "CE":
-        input.value = " 0";
-        currentInput = "";
-        previousInput = "";
-        operator = null;
-        result = null;
+    console.log("button is clicked")
+    if (input.value === '0' && !isNaN(parseInt(element.value))) {
+      input.value = element.value;
+    } else {
+    switch (element.value) {
+      case "+":
+        checkOperator();
         break;
-
-      case "C":
-        input.value = "0";
-        currentInput = "";
-        previousInput = "";
-        operator = null;
-        result = null;
+      case "-":
+        checkOperator();
+        break;
+      case "*":
+      checkOperator();
+        break;
+      case "/":
+        checkOperator();
         break;
 
       case "=":
-        if (operator) {
-          currentInput = operate(operator, previousInput, currentInput);
-          input.value = currentInput;
-        }
+        input.value = eval(input.value);
         break;
 
-      
-       
+      case "CE":
+        input.value = "0";
+        break;
 
+        case "C":
+        input.value = "0";
+        break;
 
       default:
-        currentInput += buttonValue;
-        input.value = currentInput;
+        input.value += element.value;
         break;
     }
-  });
-});
+  }
 
+  });
+
+  function checkOperator(){
+    if (arrayofoperator.includes(input.value.charAt(input.value.length - 1))) {
+           
+      input.value = input.value.slice(0, -1) + element.value;
+    } else {
+  
+      input.value += element.value;
+    }
+  }
+});
