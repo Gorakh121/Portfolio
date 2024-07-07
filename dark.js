187c1347e49ca27f29ba9a3775de923a6259d9c0
Prayas121 <prayasbaral9800@gmail.com>
2024-07-07 22:04:14 +0545

    updated

--- dark.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById('mode').addEventListener('click', () => {
-//   const b = document.body;
-//   b.classList.toggle('light-mode');
-//   b.classList.toggle('dark-mode');
-// });
